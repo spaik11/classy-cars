2192f0e43e1f39c030c3232f2a032e53bce33fcf
Sung Paik <sung.paik@codeimmersives.com>
2019-12-10 15:24:09 -0500

    completed exercise

--- main.js
+++ main.js
@@ -1,10 +1,46 @@
 class Vehicle {
+  constructor(name, wheelCount, maxSpeed) {
+    this.name = name;
+    this.wheelCount = wheelCount;
+    this.maxSpeed = maxSpeed;
+    this.speed = 0;
+  }
+
+  accelerate() {
+    this.speed += this.maxSpeed / 3;
+
+    if (this.speed >= this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+  }
 }
 
-class Car {
+class Car extends Vehicle {
+  constructor(name, maxSpeed) {
+    super(name, 4, maxSpeed)
+  }
+
+  drive() {
+    return 'Vroom. ';
+  }
 }
 
-class Ferrari {
+class Ferrari extends Car {
+  constructor() {
+    super('Ferrari', 180)
+  }
+
+  drive() {
+    return super.drive() + 'I\'m a Ferrari!';
+  }
+
+  accelerate() {
+    this.speed += this.maxSpeed / 2;
+
+    if (this.speed >= this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+  }
 }
 
 
